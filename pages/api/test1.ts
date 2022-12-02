7792cd87b89e953e5aae4556a9764ba34329704b
jeanquark <jm.kleger@gmail.com>
2022-12-02 12:42:51 +0100

    Test deploy

--- pages/api/test1.ts
+++ pages/api/test1.ts
@@ -9,5 +9,10 @@ export default function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-	res.status(200).json({ name: 'John Doe' })
+	console.log('[api/test1] req.method: ', req.method)
+    console.log('[api/test1] req.body: ', req.body)
+	const { name } = req.body
+	console.log('[api/test1] name: ', name)
+
+	res.status(200).json({ name })
 }
